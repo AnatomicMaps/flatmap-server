98181d9118f9c3a27ba13d6c2cbcd8b6c804da38
David Brooks <dave@bcs.co.nz>
2019-01-24 12:42:20 +1300

    Correctly set file location when directly running flatmap server.

--- server.py
+++ server.py
@@ -1,22 +1,23 @@
 import os.path
 from flask import Flask, send_file
 
-LOCATION = '/www/html/celldl/flatmaps'
-
-app = Flask(__name__, static_url_path='%s/static' % LOCATION)
+options = {'LOCATION': '/www/html/celldl/flatmaps'}
+app = Flask(__name__, static_url_path='%s/static' % options['LOCATION'])
 
 @app.route('/<map>/tiles/<layer>/<z>/<x>/<y>', methods=['GET', 'POST'])
 def tiles(map, layer, z, y, x):
-    filename = '%s/%s/tiles/%s/%s/%s/%s.png' % (LOCATION, map, layer, z, x, y)
+    filename = os.path.join(options['LOCATION'], map, 'tiles', layer, z, x, '%s.png' % y)
     app.logger.debug("Checking %s", filename)
     if os.path.isfile(filename):
         return send_file(filename)
     else:
-        return send_file('%s/tiles/blank.png' % LOCATION)
+        return send_file(os.path.join(options['LOCATION'], 'static/images/blank.png'))
 
 @app.route('/<map>', methods=['GET', 'POST'])
 def index(map):
-    return send_file('%s/%s/index.html' % (LOCATION, map))
+    return send_file(os.path.join(options['LOCATION'], map, 'index.html'))
 
 if __name__ == '__main__':
-    app.run(debug=False, host='localhost', port=8000)
+    import os
+    options['LOCATION'] = os.getcwd()
+    app.run(debug=True, host='localhost', port=8000)
