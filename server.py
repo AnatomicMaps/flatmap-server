2a87362c42bccd493f4d282b7b297b57fda02d26
David Brooks <dave@bcs.co.nz>
2019-01-24 14:26:02 +1300

    Ensure static files are served.

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 import os.path
 from flask import Flask, send_file
 
-options = {'LOCATION': '/www/html/celldl/flatmaps'}
-app = Flask(__name__, static_url_path='%s/static' % options['LOCATION'])
+options = {}
+if __name__ == '__main__':
+    import os
+    options['LOCATION'] = os.getcwd()
+else:
+    options['LOCATION'] = '/www/html/celldl/flatmaps'
+
+app = Flask(__name__, static_folder='%s/static' % options['LOCATION'])
+
 
 @app.route('/<map>/tiles/<layer>/<z>/<x>/<y>', methods=['GET', 'POST'])
 def tiles(map, layer, z, y, x):
@@ -13,11 +20,11 @@ def tiles(map, layer, z, y, x):
     else:
         return send_file(os.path.join(options['LOCATION'], 'static/images/blank.png'))
 
+
 @app.route('/<map>', methods=['GET', 'POST'])
 def index(map):
     return send_file(os.path.join(options['LOCATION'], map, 'index.html'))
 
+
 if __name__ == '__main__':
-    import os
-    options['LOCATION'] = os.getcwd()
     app.run(debug=True, host='localhost', port=8000)
