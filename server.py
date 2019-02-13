e1752befe22352ef911c9f71d8fbd459ea947d3e
David Brooks <dave@bcs.co.nz>
2019-02-14 12:49:15 +1300

    Update copyright and description.

--- server.py
+++ server.py
@@ -1,4 +1,25 @@
+#===============================================================================
+#
+#  Flatmap viewer and annotation tool
+#
+#  Copyright (c) 2019  David Brooks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===============================================================================
+
 import os.path
+import logging
 from flask import Flask, abort, request, send_file
 from flask_restful import Resource, Api
 from flask_expects_json import expects_json
