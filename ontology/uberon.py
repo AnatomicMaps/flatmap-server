0dd3f47ecde8e9ff7d9512966c45a1e62a49ced7
David Brooks <dave@bcs.co.nz>
2020-09-28 12:36:42 +1300

    Update helper script to show Uberon anatomical systems.

--- ontology/uberon.py
+++ ontology/uberon.py
@@ -7,23 +7,34 @@ uberon = owl.get_ontology('./uberon.owl').load()
 
 obo = uberon.get_namespace("http://purl.obolibrary.org/obo/")
 
+ANATOMICAL_SYSTEM = 'UBERON_0000467'
+
 summary = {}
+anatomical_systems = []
 for c in uberon.classes():
     if c.name.startswith('UBERON_'):
         is_a = []
         part_of = []
-        for part in c.is_a:
-            if (isinstance(part, owl.entity.ThingClass)
-              and part.name.startswith('UBERON_')):
-                is_a.append(part.name)
-            elif (isinstance(part, owl.class_construct.Restriction)
-              and part.property == obo.BFO_0000050
-              and part.value.name.startswith('UBERON_')):
-                part_of.append(part.value.name)
+        for super_class in c.is_a:
+            if (isinstance(super_class, owl.entity.ThingClass)
+              and super_class.name.startswith('UBERON_')):
+                is_a.append(super_class.name)
+            elif (isinstance(super_class, owl.class_construct.Restriction)
+              and super_class.property == obo.BFO_0000050
+              and super_class.value.name.startswith('UBERON_')):
+                part_of.append(super_class.value.name)
+
+        if ANATOMICAL_SYSTEM in is_a and len(part_of) == 0:
+          anatomical_systems.append(c.name)
+
         summary[c.name] = { 'is_a': is_a,
-                            'label': c.label,
+                            'label': c.label[0] if c.label else '',
                             'part_of': part_of,
                           }
 
 with open('uberon.json', 'w') as fp:
     json.dump(summary, fp, indent=4)
+
+print('Anatomical Systems:')
+for id in anatomical_systems:
+  print('  {}  {}'.format(id, summary[id]['label'] if id in summary else ''))
