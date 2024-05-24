144b981a3f55d6c24cc97bfe328983358c63ebec
David Brooks <d.brooks@auckland.ac.nz>
2024-05-24 13:07:57 +1200

    Tweak publication script.

--- release.sh
+++ release.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-git stash
+#git stash
 poetry build -f wheel
+
+git push origin
+git push origin v$1
 gh release create v$1 --verify-tag --title "Release $1" --notes ""
 gh release upload v$1 dist/mapserver-$1-py3-none-any.whl
-git stash pop --quiet
+#git stash pop --quiet
