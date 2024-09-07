0111aff5cb1e8364e6e8ab5e39694a42bb0c0061
David Brooks <d.brooks@auckland.ac.nz>
2024-09-07 21:25:11 +1200

    Extend `sckan_knowledge` tool to extract and restore connectivity as JSON and to list known knowledge sources.

--- tools/sckan_connectivity.py
+++ tools/sckan_connectivity.py
@@ -26,20 +26,23 @@ from tqdm import tqdm
 
 #===============================================================================
 
-import mapknowledge
+from mapknowledge import KnowledgeStore
 
 #===============================================================================
 
-def main(store_directory, knowledge_store, sckan_version, scicrunch_key):
-    store = mapknowledge.KnowledgeStore(
-        store_directory=store_directory,
-        knowledge_base=knowledge_store,
-        sckan_version=sckan_version,
-        scicrunch_key=scicrunch_key)
-
-    if store.db is None:
-        logging.error(f'Unable to open knowledge store {store_directory}/{knowledge_store}')
+def load(args):
+    scicrunch_key = os.environ.get('SCICRUNCH_API_KEY')
+    if scicrunch_key is None:
+        logging.error('Undefined SCICRUNCH_API_KEY -- cannot load SCKAN knowledge')
         exit(1)
+
+    store = KnowledgeStore(
+        store_directory=args.store_directory,
+        knowledge_base=args.knowledge_store,
+        sckan_version=args.sckan,
+        scicrunch_key=scicrunch_key)
+    if store.db is None:
+        raise IOError(f'Unable to open knowledge store {args.store_directory}/{args.knowledge_store}')
 
     knowledge_source = store.source
     logging.info(f'Loading SCKAN connectivity for source `{knowledge_source}`')
@@ -54,13 +57,100 @@ def main(store_directory, knowledge_store, sckan_version, scicrunch_key):
         unit='path', ncols=80,
         bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}')
 
+    path_count = 0
     for path in paths:
         store.entity_knowledge(path, source=knowledge_source)
         progress_bar.update(1)
+        path_count += 1
 
     store.close()
     progress_bar.close()
-    logging.info(f'Loaded connectivity for {len(paths)} paths')
+    logging.info(f'Loaded connectivity for {path_count} paths for `{knowledge_source}`')
+
+    # Having loaded connectivity we can now save it to JSON
+    if args.json_file:
+        args.source = knowledge_source
+        extract(args)
+
+#===============================================================================
+
+def extract(args):
+    knowledge_source = args.source
+    store = KnowledgeStore(
+        store_directory=args.store_directory,
+        knowledge_base=args.knowledge_store,
+        read_only=True,
+        use_scicrunch=False,
+        use_npo=False,
+        knowledge_source=knowledge_source)
+    if store.db is None:
+        raise IOError(f'Unable to open knowledge store {args.store_directory}/{args.knowledge_store}')
+
+    saved_knowledge = {
+        'source': knowledge_source,
+        'knowledge': []
+    }
+    for row in store.db.execute('select entity, knowledge from knowledge where source=?', (knowledge_source,)).fetchall():
+        knowledge = json.loads(row[1])
+        knowledge['id'] = row[0]
+        saved_knowledge['knowledge'].append(knowledge)
+
+    with open(args.json_file, 'w') as fp:
+        json.dump(saved_knowledge, fp, indent=4)
+    logging.info(f"Saved {len(saved_knowledge['knowledge'])} records for `{knowledge_source}` to `{args.json_file}`")
+
+#===============================================================================
+
+def restore(args):
+    with open(args.json_file) as fp:
+        saved_knowledge = json.load(fp)
+    store = KnowledgeStore(
+        store_directory=args.store_directory,
+        knowledge_base=args.knowledge_store,
+        use_scicrunch=False,
+        use_npo=False)
+    if store.db is None:
+        raise IOError(f'Unable to open knowledge store {args.store_directory}/{args.knowledge_store}')
+
+    knowledge_source = saved_knowledge['source']
+    store.clean_connectivity(knowledge_source)
+    for knowledge in saved_knowledge['knowledge']:
+        entity = knowledge['id']
+        store.db.execute('begin')
+        store.db.execute('replace into knowledge (source, entity, knowledge) values (?, ?, ?)',
+                                             (knowledge_source, entity, json.dumps(knowledge)))
+        # Save label and references in their own tables
+        if 'label' in knowledge:
+            store.db.execute('replace into labels values (?, ?)', (entity, knowledge['label']))
+        if 'references' in knowledge:
+            references = knowledge.get('references', [])
+            store.db.execute('delete from publications where entity = ?', (entity, ))
+            store.db.executemany('insert into publications(entity, publication) values (?, ?)',
+                ((entity, reference) for reference in references))
+        if 'connectivity' in knowledge:
+            seen_nodes = set()
+            for edge in knowledge['connectivity']:
+                for node in edge:
+                    node = (node[0], tuple(node[1]))
+                    if node not in seen_nodes:
+                        seen_nodes.add(node)
+                        store.db.execute('insert or replace into connectivity_nodes (source, node, path) values (?, ?, ?)',
+                                                                              (knowledge_source, json.dumps(node), entity))
+        store.db.commit()
+
+    logging.info(f"Restored {len(saved_knowledge['knowledge'])} records for `{knowledge_source}` from `{args.json_file}`")
+
+#===============================================================================
+
+def info(args):
+    store = KnowledgeStore(
+        store_directory=args.store_directory,
+        knowledge_base=args.knowledge_store,
+        read_only=True,
+        use_scicrunch=False,
+        use_npo=False)
+    for source in store.knowledge_sources():
+        print(source)
 
 #===============================================================================
 
@@ -71,22 +161,33 @@ if __name__ == '__main__':
 
     import argparse
 
-    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Load, extract, and restore SCKAN connectivity knowledge in a local knowledge store.')
+    parser.add_argument('--store-directory', required=True, help='Directory containing a knowledge store')
+    parser.add_argument('--knowledge-store', default=DEFAULT_STORE, help=f'Name of knowledge store file. Defaults to `{DEFAULT_STORE}`')
+    parser.add_argument('-q', '--quiet', action='store_true', help='Suppress INFO log messages')
 
-    scicrunch_key = os.environ.get('SCICRUNCH_API_KEY')
-    if scicrunch_key is None:
-        logging.error('Undefined SCICRUNCH_API_KEY -- cannot load SCKAN knowledge')
-        exit(1)
+    subparsers = parser.add_subparsers(title='commands', required=True)
 
-    parser = argparse.ArgumentParser(description='Load connectivity knowledge from SCKAN into a local knowledge store.')
-    parser.add_argument('--store-directory', required=True, help='Directory containing a knowledge store.')
-    parser.add_argument('--knowledge-store', default=DEFAULT_STORE, help=f'Name of knowledge store file. Defaults to `{DEFAULT_STORE}`.')
-    parser.add_argument('--sckan-release', help='SCKAN release identifier. Defaults to  latest released version of SCKAN.')
+    parser_load = subparsers.add_parser('load', help='Load connectivity knowledge from SCKAN into a local knowledge store')
+    parser_load.add_argument('--sckan', help='SCKAN release identifier. Defaults to latest available version of SCKAN')
+    parser_load.add_argument('--json', dest='json_file', metavar='JSON_FILE', help='Optionally save connectivity knowledge as JSON.')
+    parser_load.set_defaults(func=load)
+
+    parser_extract = subparsers.add_parser('extract', help='Extract connectivity knowledge from a local store as JSON')
+    parser_extract.add_argument('--source', help='Knowledge source to extract. Defaults to latest version')
+    parser_extract.add_argument('json_file', metavar='JSON_FILE', help='File to save connectivity knowledge to.')
+    parser_extract.set_defaults(func=extract)
+
+    parser_restore = subparsers.add_parser('restore', help='Restore connectivity knowledge to a local store from JSON')
+    parser_restore.add_argument('json_file', metavar='JSON_FILE', help='File to load connectivity knowledge from.')
+    parser_restore.set_defaults(func=restore)
+
+    parser_info = subparsers.add_parser('info', help='List knowledge sources in a local store')
+    parser_info.set_defaults(func=info)
+
     args = parser.parse_args()
-
-    main(store_directory=args.store_directory,
-        knowledge_store=args.knowledge_store,
-        sckan_version=args.sckan_release,
-        scicrunch_key=scicrunch_key)
+    if not args.quiet:
+        logging.basicConfig(level=logging.INFO)
+    args.func(args)
 
 #===============================================================================
