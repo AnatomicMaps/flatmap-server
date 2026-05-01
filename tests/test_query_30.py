3511e6d648cb827e083a38eb07548e98e3038f4a
napakalas <yuda.munarko@gmail.com>
2026-05-01 16:00:12 +1200

    cq: Add query_30 and tests for nodes under anatomical terms([#64).

---
+++ tests/test_query_30.py
@@ -0,0 +1,38 @@
+import pytest
+from utility import cq_request, assert_valid_query_response, MALE_UUID, FEMALE_UUID, RAT_UUID, SCKAN_VERSION, CQ_END_POINT
+
+base_query = {
+    'query_id': '30',
+    'parameters': [
+        {'column': 'feature_id', 'value': 'UBERON:0002080'}
+    ]
+}
+
+human_expected_node_ids = [
+    '["UBERON:0002165", ["UBERON:0002080"]]',
+    '["UBERON:0002349", ["UBERON:0002080"]]'
+]
+
+def test_human_male_map():
+    query = {**base_query, 'parameters': base_query['parameters'] + [{'column': 'source_id', 'value': MALE_UUID}]}
+    response = cq_request(query)
+    assert_valid_query_response(
+        response,
+        expected_column_values={'node_id': human_expected_node_ids}
+    )
+
+def test_human_female_map():
+    query = {**base_query, 'parameters': base_query['parameters'] + [{'column': 'source_id', 'value': FEMALE_UUID}]}
+    response = cq_request(query)
+    assert_valid_query_response(
+        response,
+        expected_column_values={'node_id': human_expected_node_ids}
+    )
+
+def test_human_rat_map():
+    query = {**base_query, 'parameters': base_query['parameters'] + [{'column': 'source_id', 'value': RAT_UUID}]}
+    response = cq_request(query)
+    assert_valid_query_response(
+        response,
+        expected_column_values={'node_id': human_expected_node_ids}
+    )
